ea6b8248bbbecc7b5e86d8fafdc68225efc310d1
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-07-22 10:19:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+  for (let i = 0; i < gifts.length; i++){
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+
+}
+
+function writeCards(names, event){
+  const messages = [];
+  for (let i = 0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+  return messages;
+}
+
+function countDown(number){
+  while (number > -1){
+    console.log(number);
+    number -= 1;
+  }
+}
+
+wrapGifts(gifts);
